9ee3ddf4b1db28e4c9c593cd569c7b48089ed25e
Zoe Koulouris <zoe@upstate.agency>
2018-12-13 13:35:53 -0500

    feat(model):

---
+++ api/controllers/SubCategoryController.js
@@ -0,0 +1,12 @@
+/**
+ * SubCategoryController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
